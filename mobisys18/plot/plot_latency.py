7b364ccde259621dbcdbf3a138494bc3edf3d03b
Kaifei Chen <kaifei@berkeley.edu>
2017-11-30 06:35:08 +0000

    update camera data with acquireNextImage, instead of acquireLatestImage

--- mobisys18/plot/plot_latency.py
+++ mobisys18/plot/plot_latency.py
@@ -21,10 +21,34 @@ def plot_cdf(data, fmt, label):
     plt.plot(bin_edges[1:], ecdf, fmt, label=label)
 
 path = sys.argv[1]
-gyro = getLatencyList(os.path.join(path, "gyroDelay.txt"))
-mag = getLatencyList(os.path.join(path, "magDelay.txt"))
-acc = getLatencyList(os.path.join(path, "accDelay.txt"))
-img = getLatencyList(os.path.join(path, "imageLantency.txt"))
+gyro = getLatencyList(os.path.join(path, "gyroDelay.txt"))[-1000:]
+mag = getLatencyList(os.path.join(path, "magDelay.txt"))[-1000:]
+acc = getLatencyList(os.path.join(path, "accDelay.txt"))[-1000:]
+img = getLatencyList(os.path.join(path, "imageLantency.txt"))[-1000:]
+
+print "gyro:"
+print "mean: ", np.mean(gyro)
+print "std: ", np.std(gyro)
+print "median: ", np.percentile(gyro, 50)
+print "99th: ", np.percentile(gyro, 99)
+
+print "mag:"
+print "mean: ", np.mean(mag)
+print "std: ", np.std(mag)
+print "median: ", np.percentile(mag, 50)
+print "99th: ", np.percentile(mag, 99)
+
+print "acc:"
+print "mean: ", np.mean(acc)
+print "std: ", np.std(acc)
+print "median: ", np.percentile(acc, 50)
+print "99th: ", np.percentile(acc, 99)
+
+print "img:"
+print "mean: ", np.mean(img)
+print "std: ", np.std(img)
+print "median: ", np.percentile(img, 50)
+print "99th: ", np.percentile(img, 99)
 
 fig = plt.figure(figsize=(12, 5))
 plot_cdf(gyro, 'r--', "Gyroscope")
@@ -37,4 +61,4 @@ plt.ylabel("CDF", fontsize=22)
 plt.grid()
 plt.savefig("sensor_latency.pdf", bbox_inches='tight')
 plt.tight_layout()
-plt.show()
+#plt.show()
